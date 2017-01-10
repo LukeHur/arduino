ecb8510d531e5358e628ee93bc9a7d6944867b4f
LukeHur <lukehur@seoulchristian.org>
2017-01-10 10:33:47 +0900

    External LED setup

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  digitalWrite(12, HIGH);// Make the light to go on
+  delay(1000); // wait for 1 second
+  digitalWrite(12, LOW); // Make the light to go off
+  delay(1000); // keep it off for 1 second
+  digitalWrite(12, HIGH); // Make the light to go on 
 }
