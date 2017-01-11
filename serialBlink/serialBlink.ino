6806d94d1e2d2968b88128b79a6f1c27e46e830c
LukeHur <lukehur@seoulchristian.org>
2017-01-11 10:18:18 +0900

    Activate the serial connection to make another LED blink

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,12 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);// Make the light to go on
-  delay(1000); // wait for 1 second
-  digitalWrite(13, LOW); // Make the light to go off
-  delay(1000); // keep it off for 1 second
+  digitalWrite(13, HIGH);
+  Serial.println("On");
+  delay(1000);
+  Serial.println("Off");
+  delay(1000);
 }
