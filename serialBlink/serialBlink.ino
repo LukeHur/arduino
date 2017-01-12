4b127231e23aabb71e13a6dfbcb070e8e4a259cc
LukeHur <lukehur@seoulchristian.org>
2017-01-12 10:06:57 +0900

    Create a system that follows my command on or off

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -2,15 +2,17 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello, I'm nice");
+  Serial.println("Send 'On' to turn on the LED.");
+  Serial.println("Send 'Off' to turn off the LED.");
   pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.readString() == "On") {
   digitalWrite(13, HIGH);
-  Serial.println("On");
-  delay(1000);
-  digitalWrite (13, LOW);
-  Serial.println("Off");
-  delay(1000);
+  }
+  else if (Serial.readString() == "Off") {
+  digitalWrite(13, LOW);
+  }
 }
