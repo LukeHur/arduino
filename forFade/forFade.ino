5e55413bf22dd9e3cfd7e291b417af1fc8c83a64
LukeHur <lukehur@seoulchristian.org>
2017-01-23 10:45:41 +0900

    Add comment to every code

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,17 +1,17 @@
 void setup() {
-  Serial.begin(9600);
-  pinMode (9, OUTPUT);
+  Serial.begin(9600); // set serial connection at baud rate at 9600
+  pinMode (9, OUTPUT); // set pin 9 to output
 }
 
 void loop() {
-  for (int lightValue = 0; lightValue <= 255; lightValue++) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+  for (int lightValue = 0; lightValue <= 255; lightValue++) { // increase the lightValue until 255
+    analogWrite(9, lightValue); // Set the brightness of LED lightValue
+    Serial.println(lightValue); // print lightValue to the serial monitor
+    delay(10); // wait for ten millisecond
   }
-  for (int lightValue = 255; lightValue >= 0; lightValue--) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+  for (int lightValue = 255; lightValue >= 0; lightValue--) { // decrease the lightValue until 0
+    analogWrite(9, lightValue); // Set the brightness of LED lightValue
+    Serial.println(lightValue); // print lightValue to the serial monitor
+    delay(10); // wait for ten millisecond
   }
 }
--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -1,17 +1,17 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; // set lightValue to 0
+int delta = 1; // set delta to 1
 
 void setup() {
-  Serial.begin(9600);
-  pinMode (9, OUTPUT);
+  Serial.begin(9600); // set serial connection at baud rate at 9600
+  pinMode (9, OUTPUT); // set pin 9 to output
 }
 
   void loop() {
-    lightValue = lightValue + delta;
-    if (lightValue <= 0 || lightValue >= 255) {
-      delta = -delta;
+    lightValue = lightValue + delta; // set the value of lightValue to the sum of lightValue and delta
+    if (lightValue <= 0 || lightValue >= 255) { // // increase the lightValue until 255
+      delta = -delta; // set delta to negative 
     }
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+    analogWrite(9, lightValue); // Set the brightness of LED lightValue
+    Serial.println(lightValue); // print lightValue to the serial monitor
+    delay(10); // wait for ten millisecond
   }
